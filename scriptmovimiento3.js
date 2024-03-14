ce1e69ef97ff65c31e356ebf350ed0a8d5cd83d7
isidrojavier <isidroj_fernandez@hotmail.com>
2024-03-14 09:34:56 +0100

    ejercicio terminado

--- scriptmovimiento3.js
+++ scriptmovimiento3.js
@@ -13,40 +13,40 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-        this.image.src = 'enemy4.png';
+        this.image.src = 'enemy3.png';
         
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 213;
-        this.spriteHeight = 213;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
         this.width = this.spriteWidth / 2;
         this.height = this.spriteHeight / 2;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
-        this.newX = Math.random() * (canvas.width - this.width);
-        this.newY = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.interval = Math.floor(Math.random() * 200 + 50); //utilizamos Math.floor para obtener numeros enteros, sino no funciona
+        //this.angle = Math.random() * 2;
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 1.5 + 0.5; //velocidad
+        //this.curve = Math.random() * 200 + 50; //jugando con el número conseguimos más angulo o no
     }
 
     update(){
-        //if (gameFrame % 100 === 0){ //cambiamos velocidad
-        if (gameFrame % this.interval === 0){ //hacemos que cada marciano se mueva diferente
-            this.newX = Math.random() * (canvas.width - this.width);
-            this.newY = Math.random() * (canvas.height - this.height); 
-        }
+        //si pongo en el seno un valor más grande que el coseno el movimiento
+        //será de arriba a abajo sinusoidalmente
+        //si el coseno es más grande, el movimiento será de izda a dcha
+        //podemos intercambiar seno y coseno en las dos líneas para ver diferencias
+        //experimentar con los angulos en PI y los senos y cosenos para ver diferentes movimientos        
+        //this.x = this.curve * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
+        this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
+        //this.y = this.curve * Math.cos(this.angle * Math.PI/180) + (canvas.height/2 - this.height/2);
+        this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/270) + (canvas.height/2 - this.height/2);
+        this.angle += this.angleSpeed;
+        //this.angle += 0.1;
+        //this.x += Math.random() * 5 - 2.5;
+        //this.y += Math.random() * 5 - 2.5;
 
-        let dx = this.x - this.newX;
-        let dy = this.y - this.newY;
-        //desaparecen y aparecen en un punto aleatorio
-        //this.x -= dx;
-        //this.y -= dy;
-        //viajan en x e y de otra forma
-        this.x -= dx/70; 
-        this.y -= dy/70;
-        
         if (this.x + this.width <0) this.x = canvas.width;
-        //animar sprites
+
         if (gameFrame % this.flapSpeed === 0){
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
